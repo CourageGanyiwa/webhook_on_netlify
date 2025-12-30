3ff32346a5032d77664f2df4cf9efd3f071edff2
CourageGanyiwa <couresma@gmail.com>
2025-12-30 03:59:19 +0200

    first commit

---
+++ functions/discorder/discorder.js
@@ -0,0 +1,26 @@
+const axios = require("axios").default;
+
+exports.handler = async (event, context) => {
+  try {
+    const body = JSON.parse(event.body);
+    const username = body.sender.login;
+    const avatarUrl = body.sender.avatar_url;
+    const repoName = body.repository.name;
+    const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+      content: ` ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
+      embeds: [
+        {
+          image: {
+            url: avatarUrl,
+          },
+        },
+      ],
+    });
+    console.log("Submitted!");
+    return {
+      statusCode: 204,
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+};
